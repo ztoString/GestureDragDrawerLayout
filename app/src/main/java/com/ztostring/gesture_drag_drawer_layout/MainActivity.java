6179ff32a9b68a1edcda2c025d56f59e01ffad99
zoutong <zoutong@changba.com>
2022-01-06 16:23:53 +0800

    init

---
+++ app/src/main/java/com/ztostring/gesture_drag_drawer_layout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ztostring.gesture_drag_drawer_layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
